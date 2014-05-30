6996aabfb2caf03219158bb2c1b0ed58a70f8d8f
Justin Donato <justin.donato@gmail.com>
2014-05-29 22:21:28 -0400

    saving work

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def exercises(user_id):
     if not user:
         return redirect('/')
 
-    return render_template('exercises.html', user=user)
+    return render_template('app.html', user=user)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
