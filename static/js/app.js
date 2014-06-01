3f71b4f47b26819c2b4b7a396f83b1d961197f78
Justin Donato <justin.donato@gmail.com>
2014-05-31 20:36:51 -0400

    saving work

--- static/js/app.js
+++ static/js/app.js
@@ -1,138 +1,393 @@
 (function() {
 
-    function getTemplate(id) {
-        return _.template($(id).html())
-    }
+    var B = Backbone;
+    var M = B.Marionette;
 
-    var ExerciseEditor = function(options) {
-        this.overlay = options.overlay;
-        this.bus = options.bus;
-        this.editBtns = options.editBtns;
-        this.addBtn = options.addBtn;
+    window.user = new B.Model(Context);
+    window.exercises = new B.Collection(Context.exercises);
 
-        this.contentTpl = getTemplate('#overlay-content-tpl');
-        this.editHeaderTpl = getTemplate('#edit-overlay-header-tpl');
-        this.editFooterTpl = getTemplate('#edit-overlay-footer-tpl');
-
-        this.createHeaderTpl = getTemplate('#create-overlay-header-tpl');
-        this.createFooterTpl = getTemplate('#create-overlay-footer-tpl');
-
-        this.newFormData = {
+    var Exercise = B.Model.extend({
+        idAttribute: 'eid',
+        defaults: {
             eid: null,
-            name: null,
             quantity: null,
+            name: null,
             value: null
-        };
-        this.initEvents();
-    }
+        }
+    });
 
-    _.extend(ExerciseEditor.prototype, {
+    var Header = M.ItemView.extend({
 
-        initEvents: function() {
-            this.overlay.mask.on('click', '.save', _.bind(this.saveExercise, this));
-            this.editBtns.on('click', _.bind(this.openEditOverlay, this));
-            this.addBtn.on('click', _.bind(this.openCreateOverlay, this));
+        template: '#tpl-header',
+
+        ui: {
+            loadingBar: '.loading-bar'
         },
 
-        saveExercise: function() {
-            var form = this.overlay.mask.find('.edit-form');
-            form.submit();
+        startLoading: function() {
+            this.ui.loadingBar
+                .removeClass('loading-done')
+                .addClass('loading-one');
         },
 
-        openEditOverlay: function(e) {
-            var data = $(e.currentTarget).data();
-            this.overlay.setHeader(this.editHeaderTpl(data));
-            this.overlay.setContent(this.contentTpl(data));
-            this.overlay.setFooter(this.editFooterTpl(data));
-            this.overlay.open();
-        },
+        finishLoading: function() {
+            this.ui.loadingBar
+                .removeClass('loading-one')
+                .addClass('loading-done');
+        }
+    });
 
-        openCreateOverlay: function() {
-            var data = this.newFormData;
-            overlay.setHeader(this.createHeaderTpl(data));
-            overlay.setContent(this.contentTpl(data));
-            overlay.setFooter(this.createFooterTpl(data));
-            overlay.open();
+    var ExerciseView = M.ItemView.extend({
+        events: {
+            'click .description': 'onClickEdit'
+        },
+        tagName: 'div',
+        className: 'exercise',
+        template: '#tpl-exercise',
+
+        onClickEdit: function() {
+            App.commands.execute('show-modal', new CreateExerciseView({model: this.model}));
         }
 
     });
 
-    var overlay = new Overlay({mask: '#overlay'});
-    var quickLogOverlay = new Overlay({mask: '#ql-overlay'});
-
-
-    var editor = new ExerciseEditor({
-        overlay: overlay,
-        editBtns: $('.edit-exercise'),
-        addBtn: $('.add-btn'),
-        bus: $('#bus'),
+    var ExerciseList = M.CollectionView.extend({
+        itemView: ExerciseView
     });
 
-    var quickLog = $('.quick-log');
+    var Body = M.ItemView;
 
-    quickLog.on('click', function(e) {
-        quickLogOverlay.open();
-    });
+    var MainNav = M.ItemView.extend({
 
-    function DepositForm(form, bus) { 
-        this.form = $(form); 
-        this.button = this.form.find('button'); 
-        this.bus = $(bus); this.initEvents(); 
-    } 
-
-    DepositForm.prototype = { 
-
-        initEvents: function() { 
-            this.form.on('submit', $.proxy(this.onSubmit, this)); 
-        }, 
-
-        onSubmit: function(e) { 
-            e.preventDefault(); 
-            this.displayWait(); 
-            this.postRequest();
-        }, 
-
-        postRequest: _.throttle(function() {
-            var req = $.ajax({ 
-                type: 'post', 
-                dataType: 'json', 
-                url: this.form.attr('action'), 
-                data: this.form.serialize() 
-            }); 
-            req.done($.proxy(this.onDone, this)); 
-            req.fail($.proxy(this.onFail, this)); 
-        }, 300),
-
-        displayWait: function() { 
-             this.button.addClass('waiting'); 
-        }, 
-
-        displayReady: function() { 
-             this.button.removeClass('waiting'); 
-        }, 
-
-        onDone: function(result) {
-            var newBalance = result.balance;
-            // TODO make this a  little more robust
-            this.bus.trigger('new-balance', newBalance);
-            this.displayReady();
+        events: {
+            'click [data-panel-trigger]': 'showPanel'
         },
 
-        onFail: function(e) {
-            this.displayReady();
+        ui: {
+            activeIndicator: '.active-indicator'
+        },
+
+        initialize: function() {
+            this.bindUIElements();
+        },
+
+        showPanel: function(e) {
+            var panel = $(e.currentTarget).data('panel-trigger');
+            App.commands.execute('show-panel', panel);
+        },
+
+        activateTab: function(index) {
+            var triggers = this.$el.find('[data-panel-trigger]');
+
+            $(triggers.removeClass('active').get(index))
+                .addClass('active');
+            
+            // TODO: make more generic
+            var leftPos = index * 50 + '%'
+            this.ui.activeIndicator.css({ left: leftPos });
+        },
+
+    });
+
+    var StatsPanel = M.Layout.extend({
+        template: '#tpl-stats-panel',
+    });
+
+    var QuickLogView = M.ItemView.extend({
+        template: '#tpl-quick-log-form'
+
+    });
+    var CreateExerciseView = M.ItemView.extend({
+        template: '#tpl-create-exercise-form'
+    });
+
+    var ExercisePanel = M.Layout.extend({
+
+        events: {
+            'click [data-trigger="quick-log"]': 'onTriggerQuickLog',
+            'click [data-trigger="create-exercise"]': 'onTriggerCreateExercise',
+        },
+
+        onTriggerQuickLog: function() {
+            App.commands.execute('show-modal', new QuickLogView());
+        },
+
+        onTriggerCreateExercise: function() {
+            App.commands.execute('show-modal', new CreateExerciseView({model: new Exercise()}));
+        },
+
+        regions: {
+            Exercises: '.exercises',
+        },
+
+        template: '#tpl-exercise-panel',
+
+        onShow: function() {
+            this.Exercises.show(new ExerciseList({collection: exercises}));
         }
 
-    }
-
-    $('.deposit-form').each(function(index, elem) {
-        new DepositForm(elem, document.body);
     });
 
-    $(document.body).on('new-balance', function(e, balance) {
-        $('.icon-chart-bar').removeClass('positive negative').addClass(balance >= 0 ? 'positive' : 'negative');
-        $('.balance-amount').html(balance);
-        quickLogOverlay.close();
+    var PanelLayout = M.Layout.extend({
+
+        events: {
+            'touchstart'          : 'handleTouchStart',
+            'touchmove'           : 'handleTouchMove',
+            'touchend'            : 'handleTouchEnd',
+            'touchcancel'         : 'handleTouchEnd',
+            'webkitTransitionEnd' : 'handleTransitionEnd',
+        },
+
+        ui: {
+            panelWrapper: '.panel-wrapper'
+        },
+
+        regions: {
+            PanelWrapper: '.panel-wrapper',
+            ExercisePanel: '.exercise-panel',
+            StatsPanel: '.stats-panel',
+        },
+
+        initialize: function() {
+            this.index = 0;
+
+            this.bindUIElements();
+            this.ExercisePanel.show(new ExercisePanel),
+            this.StatsPanel.show(new StatsPanel({model: user}))
+            this.layout();
+        },
+
+        layout: function() {
+            this.x = 0;
+            this.width = this.$el.width();
+            this.offset = this.$el.offset();
+            this.ExercisePanel.$el.css({width: this.width});
+            this.StatsPanel.$el.css({width: this.width});
+            this.ui.panelWrapper.css({width: this.width * 2});
+        },
+
+        translateStr: function(deltax) {
+            return 'translate3d(' + deltax + 'px, 0, 0)';
+        },
+
+        sub: function(a, b) {
+            var
+                ax = a.x || a.clientX || a.left,
+                bx = b.x || b.clientX || b.left,
+                ay = a.y || a.clientY || a.top,
+                by = b.y || b.clientY || b.top;
+
+            return { x: ax - bx, y: ay - by};
+        },
+
+        handleTransitionEnd: function() {
+            this.transitionOff();
+        },
+
+        handleTouchStart: function(e) {
+            var touch = e.originalEvent.targetTouches[0];
+            this.startCoords = this.sub(touch, this.offset)
+            this.lastCoords = this.startCoords;
+        },
+
+        handleTouchMove: function(e) {
+
+            var touch = e.originalEvent.changedTouches[0];
+            var touchOffset = this.sub(touch, this.offset)
+            var totalDiff = this.sub(this.startCoords, touchOffset);
+
+            if (Math.abs(totalDiff.x) < 10) {
+                return;
+            }
+
+            var diff = this.sub(touchOffset, this.lastCoords);
+
+            if (diff.x < 0) {
+
+                if (this.hasNext()) {
+                    this.translate(diff.x);
+                } else {
+                    return;
+                }
+
+            } else {
+
+                if (this.hasPrev()) {
+                    this.translate(diff.x);
+                } else {
+                    return;
+                }
+            }
+
+            this.lastCoords = this.sub(touch, this.offset);
+        },
+
+        handleTouchEnd: function(e) {
+
+            var touch = e.originalEvent.changedTouches[0];
+            var diff = this.sub(this.startCoords, this.lastCoords);
+
+            if (Math.abs(diff.x) > this.width / 4) {
+                diff.x > 0 ? this.next() : this.prev();
+            } else {
+                this.snap(this.sub(this.startCoords, this.lastCoords).x);
+            }
+        },
+
+        hasNext: function() {
+            // TODO pass this in somehow when you extract to behavoir
+            return this.index < 1;
+        },
+
+        hasPrev: function() {
+            return this.index > 0;
+        },
+
+        next: function() {
+            this.index = this.index + 1;
+            this.transitionOn();
+            this.moveTo(this.index);
+        },
+
+        prev: function() {
+            this.index = this.index - 1;
+            this.transitionOn();
+            this.moveTo(this.index);
+        },
+
+        moveTo: function(index) {
+            App.vent.trigger('panel:change', index, this.ui.panelWrapper.children().get(index));
+            this.translateTo(-(index * this.width));
+        },
+
+        moveToByClass: function(className) {
+            this.transitionOn();
+            var index = this.$el.find("." + className).index();
+            this.moveTo(index);
+        },
+
+        translate: function(deltax) {
+            this.x = this.x + deltax;
+            this.setWrapperCss({'webkitTransform': this.translateStr(this.x)});
+        },
+
+        snap: function(deltax) {
+            this.transitionOn();
+            this.translate(deltax);
+        },
+
+        transitionOn: function() {
+            this.setTransition('-webkit-transform .3s');
+        }, 
+
+        transitionOff: function() {
+            this.setTransition('');
+        }, 
+
+        setTransition: function(transition) {
+            this.setWrapperCss({'webkitTransition': transition});
+        },
+
+        translateTo: function(x) {
+            this.x = x;
+            this.setWrapperCss({'webkitTransform': this.translateStr(this.x)})
+        },
+
+        setWrapperCss: function(css) {
+            this.ui.panelWrapper.css(css);
+        },
+
+    })
+
+    var ModalLayout = M.Layout.extend({
+
+        events: {
+            'click .close': 'onClickClose'
+        },
+
+        regions: {
+            CloseBtn: '.close',
+            Body: '.modal-body'
+        },
+
+        initialize: function() {
+            this.bindUIElements();
+        },
+
+        show: function(view) {
+            this.Body.show(view);
+            this.$el.addClass('active');
+        },
+
+        close: function() {
+            this.$el.removeClass('active');
+        },
+
+        onClickClose: function() {
+            App.commands.execute('close-modal');
+        },
+
     });
 
+    window.header = new Header({model: user});
+    var body = new Body({el: $('#body')});
+    var panelLayout = new PanelLayout({el: '.panels'});
+    var mainNav = new MainNav({el: $('.main-nav')});
+    var modal = new ModalLayout({el: $('#modal')});
+
+    var Controller = M.Controller.extend({
+
+        showPanel: function(panel) {
+            panelLayout.moveToByClass(panel);
+        },
+
+        showModal: function(view) {
+           modal.show(view);
+           App.Body.$el.addClass('receding');
+        },
+
+        closeModal: function() {
+           modal.close();
+           App.Body.$el.removeClass('receding');
+        },
+
+        updateNav: function(index) {
+            mainNav.activateTab(index);
+        },
+
+    });
+
+    window.App = new M.Application;
+
+    App.addRegions({
+        Frame: '#frame',
+        Body: '#body',
+        Header: 'header',
+        ExercisePanel: '.exercise-panel',
+        MainNav: '.main-nav',
+        Panels: '.panels',
+        Modal: '#modal',
+    });
+
+    App.addInitializer(function(options) {
+
+        var controller = new Controller();
+        App.commands.setHandler('show-panel', controller.showPanel, controller);
+        App.commands.setHandler('show-modal', controller.showModal, controller);
+        App.commands.setHandler('close-modal', controller.closeModal, controller);
+        App.vent.on('panel:change', controller.updateNav, controller);
+
+        App.Frame.ensureEl();
+        App.Body.ensureEl();
+
+        App.Header.show(header);
+        App.Body.attachView(body);
+        App.Panels.attachView(panelLayout);
+        App.MainNav.attachView(mainNav);
+        App.Modal.attachView(modal);
+
+    });
+
+    App.start();
 
 })()
